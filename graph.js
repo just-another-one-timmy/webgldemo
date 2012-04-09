15dcb071f55cb19a037847d1f0d6dba5143e76e3
Iaroslav Tymchenko <jtimchenko@gmail.com>
2012-04-09 15:18:25 +0300

    First phase of animation done (placing graph using forces method). Next step is to enable second phase of animatino (flying spheres between nodes or something like that..)

--- graph.js
+++ graph.js
@@ -2,11 +2,12 @@ var container;
 var camera;
 var scene;
 var renderer;
-var cube;
-var repulsionCoef = 20;
+var plane;
+var repulsionCoef = 14;
 var attractionCoef = .000006;
 var dampingCoef = 0.5;
 var cameraStepSize = 5;
+var minEnergyLevel = 140; 
 
 document.onkeydown = function(event) {
     switch (String.fromCharCode(event.which).toLowerCase()) {
@@ -26,118 +27,128 @@ var graph = new function() {
     this.edges = [];
 
     var lastLetter = undefined;
+    var kineticEnergy = Number.MAX_VALUE;
+    // Did we reach the min level? If no, we should continue forces calcuation,
+    // even if current level < min energy level
+    // Once we cross this level when system's total energy is decreasing,
+    // forces will no longer affect nodes
+    var reachedMinEneryLevel = false;
     
     this.getEdge = function(mark) {
-	var i, edge;
-	for (i in this.edges) {
-	    edge = this.edges[i];
-	    if (edge.mark === mark) {
-		return edge;
-	    }
-	}
+        var i, edge;
+        for (i in this.edges) {
+            edge = this.edges[i];
+            if (edge.mark === mark) {
+                return edge;
+            }
+        }
     };
 
     this.getNode = function(letter) {
-	var i, node;
-	for (i in this.nodes) {
-	    node = this.nodes[i];
-	    if (node.mark === letter) {
-		return node;
-	    }
-	}
-	return undefined;
+        var i, node;
+        for (i in this.nodes) {
+            node = this.nodes[i];
+            if (node.mark === letter) {
+                return node;
+            }
+        }
+        return undefined;
     };
 
     this.createEdge = function(mark) {
-	var edge = {mark: mark, freq: 0};
-	this.edges.push(edge);
-	return edge;
+        var edge = {mark: mark, freq: 0};
+        this.edges.push(edge);
+        return edge;
     };
 
     this.makeMesh3d = function() {
-	var geometry = new THREE.SphereGeometry(10, 32, 32),
-	material = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
-	geometry.dynamic = true;
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.position.setX((Math.random() - 0.5) * 200);
-	mesh.position.setZ((Math.random() - 0.5) * 200);
-	mesh.position.setY((Math.random() - 0.5) * 10);
-	return mesh;
+        var geometry = new THREE.SphereGeometry(10, 32, 32),
+        material = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
+        geometry.dynamic = true;
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.setX((Math.random() - 0.5) * 200);
+        mesh.position.setZ((Math.random() - 0.5) * 200);
+        mesh.position.setY((Math.random() - 0.5) * 10);
+        return mesh;
     };
     
     this.createNode = function(letter) {
-	var node = {mark: letter,
+        var node = {mark: letter,
                     freq: 0,
                     obj: this.makeMesh3d(),
                     force: new THREE.Vector3(),
                     velocity: new THREE.Vector3()
                    };
-	this.nodes.push(node);
-	node.dy = Math.random();
-	node.animate = function () {
-	    this.obj.position.y += this.dy;
-	    if (Math.abs(node.obj.position.y) > 10) {
-		this.dy = -this.dy;
-	    }
-            this.obj.position.x += this.velocity.x;
-            this.obj.position.z += this.velocity.z;
-	}
-	
-	return node;
+        this.nodes.push(node);
+        node.dy = Math.random();
+        node.animate = function () {
+            this.obj.position.y += this.dy;
+            if (Math.abs(node.obj.position.y) > 10) {
+                this.dy = -this.dy;
+            }
+
+            if (!graph.shouldStop()) {
+                this.obj.position.x += this.velocity.x;
+                this.obj.position.z += this.velocity.z;
+            }
+        }
+        node.getKineticEnergy = function () {
+            return this.velocity.lengthSq();
+        }
+        return node;
     };
 
-    // 
     this.eatLetter = function(letter) {
 
-	if (!(letter >= 'a' && letter <= 'z')) {
-	    lastLetter = undefined;
-	}
-	
-	var node, edge, scalev;
+        if (!(letter >= 'a' && letter <= 'z')) {
+            lastLetter = undefined;
+        }
 
-	node = this.getNode(letter);
-	if (node === undefined) {
-	    node = this.createNode(letter);
-	}
-	node.freq += 1;
-	node.obj.scale.addScalar(0.2);
+        var node, edge, scalev;
 
-	if (lastLetter !== undefined) {
-	    edge = this.getEdge();
-	    if (edge === undefined) {
-		edge = this.createEdge(lastLetter + letter);
-	    }
-	    edge.freq += 1;
-	    //alert(edge.toSource());
-	}
-	
-	lastLetter = letter;
+        node = this.getNode(letter);
+        if (node === undefined) {
+            node = this.createNode(letter);
+        }
+        node.freq += 1;
+        node.obj.scale.addScalar(0.2);
+
+        if (lastLetter !== undefined) {
+            edge = this.getEdge();
+            if (edge === undefined) {
+                edge = this.createEdge(lastLetter + letter);
+            }
+            edge.freq += 1;
+            //alert(edge.toSource());
+        }
+
+        lastLetter = letter;
     };
 
     this.animateNodes = function() {
-	for (var i in this.nodes) {
-	    this.nodes[i].animate();
-	}
+        for (var i in this.nodes) {
+            this.nodes[i].animate();
+        }
     };
 
     this.computeForces = function() {
-	var i, j, ipos, jpos, dist, diff, attractionForce, repulsionForce;
-	for (i in this.nodes) {
-	    for (j in this.nodes) {
-		if (i !== j) {
-		    ipos = this.nodes[i].obj.position;
-		    jpos = this.nodes[j].obj.position;
-                    attractionForce = this.calcAttractionForce(ipos, jpos);
-                    repulsionForce = this.calcRepulsionForce(ipos, jpos);
+        var i, j, ipos, jpos, dist, diff, attractionForce, repulsionForce;
+        for (i in this.nodes) {
+            for (j in this.nodes) {
+                if (i !== j) {
+                    ipos = this.nodes[i].obj.position;
+                    jpos = this.nodes[j].obj.position;
+                    attractionForce = this.calcAttractionForce(this.nodes[i], this.nodes[j]);
+                    repulsionForce = this.calcRepulsionForce(this.nodes[i], this.nodes[j]);
                     this.nodes[i].force.x += attractionForce.x + repulsionForce.x;
                     this.nodes[i].force.z += attractionForce.z + repulsionForce.z;
-		}
-	    }
-	}
+                }
+            }
+        }
     };
 
     this.applyForces = function() {
-	var i, node;
+        var i, node;
         for (i in this.nodes) {
             node = this.nodes[i];
             node.velocity.x += node.force.x;
@@ -146,11 +157,16 @@ var graph = new function() {
         }
     };
 
-    this.calcAttractionForce = function(pos1, pos2) {
-        return new THREE.Vector3(pos2.x - pos1.x, 0, pos2.z - pos1.z).multiplyScalar(attractionCoef);
+    this.calcAttractionForce = function(node1, node2) {
+        var frequency = 0, edge = this.getEdge(node1.mark+node2.mark), pos1 = node1.obj.position, pos2 = node2.obj.position;
+        if (edge !== undefined) {
+            frequency = edge.freq;
+        }
+        return new THREE.Vector3(pos2.x - pos1.x, 0, pos2.z - pos1.z).multiplyScalar(attractionCoef).multiplyScalar(frequency);
     };
 
-    this.calcRepulsionForce = function(pos1, pos2) {
+    this.calcRepulsionForce = function(node1, node2) {
+        var pos1 = node1.obj.position, pos2 = node2.obj.position;
         var sqdist = Math.pow(pos1.x - pos2.x, 2) + Math.pow(pos1.z - pos2.z, 2);
         return new THREE.Vector3(pos1.x - pos2.x, 0, pos1.z - pos2.z).
             normalize().
@@ -158,12 +174,29 @@ var graph = new function() {
             multiplyScalar(repulsionCoef);
     };
 
+    this.recalculateKineticEnergy = function() {
+        var i;
+        this.kineticEnergy = 0;
+        for (i in this.nodes) {
+            this.kineticEnergy += this.nodes[i].getKineticEnergy();
+        }
+        if (!this.reachedMinEnergyLevel && this.kineticEnergy > minEnergyLevel) {
+            this.reachedMinEnergyLevel = true;
+        }
+        console.log("Current energy = " + this.kineticEnergy);
+    }
+
+    this.shouldStop = function() {
+        return (this.reachedMinEnergyLevel && this.kineticEnergy < minEnergyLevel);
+    }
+    
     this.doAnimation = function(){
-        for (var i = 0; i < 10; i++) {
+        if (!this.shouldStop()) {
             graph.computeForces();
             graph.applyForces();
         }
         graph.animateNodes();
+        graph.recalculateKineticEnergy();
     };
 };
 
@@ -171,9 +204,10 @@ init();
 animate();
 
 function init() {
-    var msg = "Some english text here. Maybe we can do better... anyway, let's start with that for now!".toLowerCase(), i;
+    var msg = "Some english text here. Maybe we can do better... anyway, let's start with that for now!" +
+        "abcdefghijklmnopqrstuvwxyz".toLowerCase(), i;
     for (i = 0; i < msg.length; i += 1) {
-	graph.eatLetter(msg.charAt(i));
+        graph.eatLetter(msg.charAt(i));
     }
     
     container = document.createElement('div');
@@ -189,19 +223,23 @@ function init() {
     scene.add(ambientLight);
 
     var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(1, 1, 0).normalize();
+    directionalLight.position.set(1, 1, 0.5).normalize();
     scene.add(directionalLight);
-
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.x = 200;
     camera.position.z = 500;
     scene.add(camera);
 
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(600, 600, 10, 10), new THREE.MeshLambertMaterial({color: 0xdddddd}));
+    plane.rotation.x = - 90 * Math.PI / 180;
+    plane.position.y = -40;
+    scene.add(plane);
+    
     var node;
     for (i in graph.nodes) {
-	node = graph.nodes[i];
-	scene.add(node.obj);
+        node = graph.nodes[i];
+        scene.add(node.obj);
     }
     
 }
